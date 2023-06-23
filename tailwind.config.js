d2880de8834ce6a4abfed07f0539ee9f27b996c7
LucidCodes <57482590+bloms11@users.noreply.github.com>
2023-06-23 02:44:26 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: "320px",
+      md: "970px",
+      lg: "1024px",
+      xl: "1286px",
+      "2xl": "1536px",
+    },
+    extend: {
+      keyframes: {
+        bounce: {
+          "0%, 100%": {
+            transform: "translateY(-5%)",
+            "animation-timing-function": "cubic-bezier(0.8, 0, 1, 1)",
+          },
+          "50%": {
+            transform: "translateY(0)",
+            "animation-timing-function": "cubic-bezier(0, 0, 0.2, 1)",
+          },
+        },
+      },
+      animation: {
+        "bounce-anim": "bounce 1s infinite",
+      },
+    },
+  },
+};
